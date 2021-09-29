2eaf930aa21353e8920b7bb14ac57f6a117f6828
Asel-K <asel.karagazieva@gmail.com>
2021-09-28 23:01:47 -0500

    Input with label

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import InputWithLabel from './InputWithLabel';
 
 function AddTodoForm({onAddTodo}) {
     const [todoTitle, setTodoTitle] = React.useState(''); 
@@ -11,19 +12,18 @@ function AddTodoForm({onAddTodo}) {
     const handleAddTodo = (event) => {
         event.preventDefault();
         console.log(todoTitle);
-        onAddTodo({title : todoTitle, id:Date.now()}); //new todo
+        onAddTodo({title: todoTitle, id: Date.now()}); //new todo
         setTodoTitle('');
     };
     return (
         <form onSubmit={handleAddTodo}> 
-            <label htmlFor="todoTitle">Title: </label>
-            <input 
-                id="todoTitle" 
-                type="text" 
-                name="title" 
-                value={todoTitle}
-                onChange={handleTitleChange}
-            />
+            <InputWithLabel
+                todoTitle={todoTitle}
+                handleTitleChange={handleTitleChange} 
+            >
+                Title:
+            </InputWithLabel>
+                
             <button type="submit">Add</button>
         </form>   
     );
